ea8cef85a92dc5c0b2b01e1815fa5144ebf374f0
Fadi AT <fadiatamny@gmail.com>
2020-03-31 15:22:03 +0300

    Update welcome.py

--- welcomeCog/welcome.py
+++ welcomeCog/welcome.py
@@ -7,7 +7,7 @@ from redbot.core import Config, checks, commands
 from redbot.core.utils.chat_formatting import box, humanize_list, pagify
 
 url = 'https://raw.githubusercontent.com/Kanium/KanuimCogs/master/welcomeCog/data/embedded_message.json'
-allowed_guilds = {508781789737648138, 274657393936302080, 693796372092289024}
+allowed_guilds = {274657393936302080, 693796372092289024}
 
 async def fetchMessage():
     async def fetch():
