e3dcb9be0275630971cb9d6c06de73afed4d7735
Fadi Atamny <fadiatamny@gmail.com>
2020-05-10 16:41:49 +0300

    test

--- trafficCog/trafficCog.py
+++ trafficCog/trafficCog.py
@@ -7,6 +7,7 @@ allowed_guilds = {274657393936302080, 693796372092289024, 508781789737648138}
 admin_roles = {'Developer', 'admin', 'Council'}
 statsThumbnailUrl = 'https://www.kanium.org/machineroom/logomachine-small.png'
 
+
 class TrafficCog(commands.Cog):
 
     def __init__(self, bot):
@@ -21,7 +22,8 @@ class TrafficCog(commands.Cog):
 
     def __checkClock(self):
         currdate = self.date - datetime.now()
-        if currdate.days >= 0 :
+        print(currdate.days, currdate)
+        if currdate.days >= 0:
             self.dailyJoinedCount = 0
             self.dailyLeftCount = 0
             self.date = datetime.now()
@@ -48,13 +50,19 @@ class TrafficCog(commands.Cog):
     async def statistics(self, ctx: commands.Context) -> None:
         self.__checkClock()
         await ctx.trigger_typing()
-        message = discord.Embed(title='Server Traffic Stats', description='Statistics on server activity\n\n',color=0x3399ff)
+        message = discord.Embed(title='Server Traffic Stats',
+                                description='Statistics on server activity\n\n', color=0x3399ff)
         message.set_thumbnail(url=statsThumbnailUrl)
-        message.add_field(name='Daily Joined', value=self.dailyJoinedCount, inline='True')
-        message.add_field(name='Daily Left', value='{0}\n'.format(self.dailyLeftCount), inline='True')
-        message.add_field(name='Total Traffic', value=self.totalLogs, inline='False')
-        message.add_field(name='Total Joined', value=self.totalJoinedCount, inline='True')
-        message.add_field(name='Total Left', value=self.totalLeftCount, inline='True')
+        message.add_field(name='Daily Joined',
+                          value=self.dailyJoinedCount, inline='True')
+        message.add_field(name='Daily Left', value='{0}\n'.format(
+            self.dailyLeftCount), inline='True')
+        message.add_field(name='Total Traffic',
+                          value=self.totalLogs, inline='False')
+        message.add_field(name='Total Joined',
+                          value=self.totalJoinedCount, inline='True')
+        message.add_field(name='Total Left',
+                          value=self.totalLeftCount, inline='True')
         await ctx.send(content=None, embed=message)
 
     @commands.command(name='resetstats', description='Resets statistics')
@@ -84,20 +92,22 @@ class TrafficCog(commands.Cog):
                 return
             self.__checkClock()
             if self.channel in member.guild.channels and self.toggleLogs:
-                await self.channel.send('>>> {0} has joined the server'.format(member.mention))
+                await self.channel.send('{0} has joined the server'.format(member.name))
             self.totalJoinedCount += 1
             self.dailyJoinedCount += 1
             self.totalLogs += 1
         except (discord.NotFound, discord.Forbidden):
             print(
-                f'Error Occured! sending a dm to {member.display_name} didnt work !')
+                f'Error Occured!')
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member) -> None:
         try:
+            if member.guild.id not in allowed_guilds:
+                return
             self.__checkClock()
             if self.channel in member.guild.channels and self.toggleLogs:
-                await self.channel.send('>>> {0} has left the server'.format(member.mention))
+                await self.channel.send('{0} has left the server'.format(member.name))
             self.totalLeftCount += 1
             self.dailyLeftCount += 1
             self.totalLogs += 1
@@ -108,22 +118,13 @@ class TrafficCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_ban(self, guild: discord.Guild, member: discord.Member) -> None:
         try:
+            if member.guild.id not in allowed_guilds:
+                return
             self.__checkClock()
             if self.channel in member.guild.channels and self.toggleLogs:
-                await self.channel.send('>>> {0} has been banned from the server'.format(member.mention))
+                await self.channel.send('{0} has been banned from the server'.format(member.name))
             self.totalLeftCount += 1
             self.dailyLeftCount += 1
-            self.totalLogs += 1
-        except (discord.NotFound, discord.Forbidden):
-            print(
-                f'Error Occured!')
-
-    @commands.Cog.listener()
-    async def on_member_ban(self, guild: discord.Guild, member: discord.Member) -> None:
-        try:
-            self.__checkClock()
-            if self.channel in member.guild.channels and self.toggleLogs:
-                await self.channel.send('>>> {0} has been unbanned from the server'.format(member.mention))
             self.totalLogs += 1
         except (discord.NotFound, discord.Forbidden):
             print(
